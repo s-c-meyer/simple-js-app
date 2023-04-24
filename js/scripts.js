86377bdaacdfc45cedbffb8f7195e359a2e95e86
s-c-meyer <meyer.samuelc@gmail.com>
2023-04-24 10:36:18 -0700

    Updated scripts.js
    Removed code from the lesson, and added code from the task for submission

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,7 @@
 
 /*you can also comment out as in css with the slash and asterisk*/
 
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+alert('Hello World');
 
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size= 100;
-let doubleSize = size*2;
-let minSize = (doubleSize*2) - (size/2);
-document.write(minSize);
+let favoriteFood = 'ChickFilA';
+document.write(favoriteFood);
