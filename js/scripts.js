dcd7b83923d825cd0d63f4e26bf94c389c5a7f9d
s-c-meyer <meyer.samuelc@gmail.com>
2023-04-24 10:30:34 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//comments can be single line with two slashes 
+
+/*you can also comment out as in css with the slash and asterisk*/
+
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size= 100;
+let doubleSize = size*2;
+let minSize = (doubleSize*2) - (size/2);
+document.write(minSize);
