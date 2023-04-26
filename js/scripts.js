698151928a62ab9f3e7fd285be92a9b65de47a93
s-c-meyer <meyer.samuelc@gmail.com>
2023-04-26 09:35:55 -0700

    Added pokemonList array
    Erased the preliminary favorite food item from the previous lesson, and then added the pokemonList array to begin the project for this achievement.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,33 @@
 
 /*you can also comment out as in css with the slash and asterisk*/
 
-alert('Hello World');
+let pokemonList = [];
 
-let favoriteFood = 'ChickFilA';
-document.write(favoriteFood);
+pokemonList[0] = {
+    name : 'Swampert',
+    height: 1.5,
+    weight: 81.9,
+    type: ['water', 'ground']
+};
+
+pokemonList[1] = {
+    name: 'Rayquaza',
+    height: 7,
+    weight: 206.5,
+    type: ['dragon', 'flying']
+};
+
+pokemonList[2] = {
+    name: 'Ditto',
+    height: 0.3,
+    weight: 4,
+    type: ['normal']
+};
+
+pokemonList[3] = {
+    name: 'Mr. Mime',
+    height: 1.3,
+    weight: 54.5,
+    type: ['psychic', 'fairy']
+};
+
