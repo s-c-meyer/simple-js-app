bfdd031e5681eff9630b070927b63f04384fa951
s-c-meyer <meyer.samuelc@gmail.com>
2023-05-01 10:57:44 -0700

    Added for loop
    Added a for loop which prints out the pokemonList and also details which pokemon are big

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,15 @@ pokemonList[3] = {
     type: ['psychic', 'fairy']
 };
 
+let bigHeight = 5; //the height at which a pokemon is big 
+
+for (i=0; i<pokemonList.length; i++) {
+    if (pokemonList[i].height <= bigHeight) { //if the pokemon is less than the bigHeight, do not print out that it is big
+        document.write (`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) </p>`); 
+    } else { //if the pokemon is greater than the bigHeight, then print that it is big 
+        document.write (`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) - WOW, that's big! </p>`); 
+    }
+}
+
+
+
