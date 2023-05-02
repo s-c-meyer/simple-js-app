168d4b3f7e65318478d77763b8608aa760978d84
s-c-meyer <meyer.samuelc@gmail.com>
2023-05-02 10:44:04 -0700

    Added IIFE and updated forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,55 @@
-//comments can be single line with two slashes 
+let pokemonRepository = (function () { //beginning of the IIFE
+    
+    let pokemonList = [];
 
-/*you can also comment out as in css with the slash and asterisk*/
+    pokemonList[0] = {
+        name : 'Swampert',
+        height: 1.5,
+        weight: 81.9,
+        type: ['water', 'ground']
+    };
+    
+    pokemonList[1] = {
+        name: 'Rayquaza',
+        height: 7,
+        weight: 206.5,
+        type: ['dragon', 'flying']
+    };
+    
+    pokemonList[2] = {
+        name: 'Ditto',
+        height: 0.3,
+        weight: 4,
+        type: ['normal']
+    };
+    
+    pokemonList[3] = {
+        name: 'Mr. Mime',
+        height: 1.3,
+        weight: 54.5,
+        type: ['psychic', 'fairy']
+    };    
 
-let pokemonList = [];
+    //allows you to add a pokemon to the array
+    function add(pokemon) { 
+        pokemonList.push(pokemon);
+    }
 
-pokemonList[0] = {
-    name : 'Swampert',
-    height: 1.5,
-    weight: 81.9,
-    type: ['water', 'ground']
-};
+    //allows access to the array from outside of the IIFE
+    function getAll() {
+        return pokemonList;
+    }
 
-pokemonList[1] = {
-    name: 'Rayquaza',
-    height: 7,
-    weight: 206.5,
-    type: ['dragon', 'flying']
-};
+    return {
+        add: add,
+        getAll: getAll
+    };
 
-pokemonList[2] = {
-    name: 'Ditto',
-    height: 0.3,
-    weight: 4,
-    type: ['normal']
-};
-
-pokemonList[3] = {
-    name: 'Mr. Mime',
-    height: 1.3,
-    weight: 54.5,
-    type: ['psychic', 'fairy']
-};
+})();
 
 let bigHeight = 5; //the height at which a pokemon is considered big 
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height <= bigHeight) { //if the pokemon is less than the bigHeight, do not print out that it is big
         document.write (`<p> ${pokemon.name} (height: ${pokemon.height}) </p>`); 
     } else { //if the pokemon is greater than the bigHeight, then print that it is big 
@@ -42,5 +57,8 @@ pokemonList.forEach(function(pokemon) {
     }
 });
 
+pokemonRepository.add( {name: 'Pikachu'} );
+
+console.log(pokemonRepository.getAll());
 
 
